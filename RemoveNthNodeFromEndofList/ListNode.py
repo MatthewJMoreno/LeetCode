c87ed7bc9481a4460b1b71e3f0743ba41d62783b
MatthewJMoreno <mmoreno@rams.colostate.edu>
2021-08-27 19:43:03 -0600

    sol

--- RemoveNthNodeFromEndofList/ListNode.py
+++ RemoveNthNodeFromEndofList/ListNode.py
@@ -2,4 +2,3 @@ class ListNode(object):
   def __init__(self, val = 0, next=None):
     self.val = val
     self.next = next
-  
--- RemoveNthNodeFromEndofList/solution.py
+++ RemoveNthNodeFromEndofList/solution.py
@@ -5,18 +5,61 @@ Example:
   Input: head = [1,2,3,4,5], n = 2
   Output: [1,2,3,5]
 '''
-from .. import ListNode
+from ListNode import ListNode
+
+def create_linked_list(n):
+  head = ListNode
+  cur_node = head
+
+  for i in range(n):
+    cur_node.next = ListNode(i)
+    cur_node = cur_node.next
+  
+  return head.next
+
+def print_linked_list(ll):
+  output = []
+  head = ll
+
+  if head is None:
+    return ''
+
+  while head.next is not None:
+    output.append(str(head.val))
+    output.append(' -> ')
+
+    head = head.next
+
+  output.append(str(head.val))
+
+  print(''.join(output))
+  
 
 def removeNthFromEnd(head, n):
-  visited = []
+  nodes = []
   cur_node = head
 
   while cur_node != None:
-    visited.append(cur_node)
-
+    nodes.append(cur_node)
     cur_node = cur_node.next
-  
-  print(len(visited))
 
-test = ListNode(23)
-print(test.val)
+  length = len(nodes)
+
+  if n == length:
+    head = head.next
+  elif n == 1:
+    nodes[length - 2].next = None
+  else:
+    previous_node = nodes[length - n -1]
+    nodes[length - n] = None
+    next_node = nodes[length - n + 1]
+
+    previous_node.next = next_node
+
+  return head
+
+
+head = create_linked_list(2)
+print_linked_list(head)
+head = removeNthFromEnd(head, 1)
+print_linked_list(head)
